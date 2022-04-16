fbf466e8a7e56199848b5b6519e0b0bd3a06b1e2
David Delgado <david.delgadop88@gmail.com>
2022-04-16 10:39:34 -0400

    Right side, H1

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-'enable strict';
-
 const BILL = 142.55;
 const TIP_PERCENTAGE = 0.15;
 const PEOPLE = 5;
 
-const tip = function (bill, percentage, numberOfPeople) {
+
+
+function tipPerPerson(bill, percentage, numberOfPeople) {
     return (bill * percentage) / numberOfPeople;
 }
 
 function billPerPerson(bill, tip, numberOfPeople) {
     return (bill / numberOfPeople) + tip;
 }
+
+let tip = tipPerPerson(BILL, TIP_PERCENTAGE, PEOPLE);
+
+console.log(tip);
+console.log(billPerPerson(BILL, tip, PEOPLE));
