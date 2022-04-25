775ab7267cf6955f453d6c97ec624a0b585835ef
David Delgado <david.delgadop88@gmail.com>
2022-04-24 22:36:09 -0400

    JS Functionality

---
+++ GUIDLINES.md
@@ -0,0 +1,18 @@
+BILL
+
+Can't be a negative number
+Could be 0
+NO DASHES
+
+Custom Percent
+
+Can't be a negative number
+Could be 0 (Waiter gets no tip)
+
+Number of People
+
+Can't be <= 0
+
+
+Output should update as numbers are entered, no enter button.
+Numbers should be trunked to 2 decimals.
--- script.js
+++ script.js
@@ -1,18 +1,70 @@
-const BILL = 142.55;
-const TIP_PERCENTAGE = 0.15;
-const PEOPLE = 5;
+const bill = document.getElementById('bill__field');
+const tipsBtns = document.querySelectorAll('label[class=percentage][for]');
+const tipsRadios = document.querySelectorAll('input[type=radio]')
+const custom = document.getElementById('customPercent');
+const people = document.getElementById('people__field');
 
+let billValue = 0.0;
+let tipValue = 0.15;
+let customValue = 0.0;
+let peopleValue = 5;
 
+bill.addEventListener('input', setBillValue);
+custom.addEventListener('input', setCustomTip);
+people.addEventListener('input', setPeopleValue);
 
-function tipPerPerson(bill, percentage, numberOfPeople) {
-    return (bill * percentage) / numberOfPeople;
+function setBillValue() {
+    billValue = parseFloat(bill.value);
+    console.log(billValue)
 }
 
-function billPerPerson(bill, tip, numberOfPeople) {
-    return (bill / numberOfPeople) + tip;
+function setCustomTip() {
+    tipsRadios.forEach(btn => {
+        if(btn.checked == true) {
+            btn.checked = false;
+        }
+    });
+    tipValue = parseFloat(custom.value);
+    console.log(tipValue);
 }
 
-let tip = tipPerPerson(BILL, TIP_PERCENTAGE, PEOPLE);
+function setPeopleValue() {
+    peopleValue = parseFloat(people.value);
+    if(peopleValue == 0) {
+        //display error
+        //set all output to 0
+    }
+    console.log(peopleValue)
+}
 
-console.log(tip);
-console.log(billPerPerson(BILL, tip, PEOPLE));
+tipsBtns.forEach(btn => {
+    btn.addEventListener('click', function selectTip() {
+        tipValue = parseFloat(btn.textContent.trim())/100;
+        console.log(tipValue);
+    })
+});
+
+function calc() {
+    let tipPerPerson = billValue * tipValue / peopleValue;
+    let billPerPerson = billValue / peopleValue + tipPerPerson;
+}
+
+// 1. Get the Bill
+    //  1.1 Make sure Bill doesn't include dashes or symbols that aren't numbers. 
+    //  1.1.1 Display error and leave output as 0.00
+// 2. Get the Tip Percentage
+    //  2.1 If Percentage = Custom => 1.1 & 1.1.1
+// 3. Get the ammount of people
+    //  3.1 People can't be 0 nor negative => 1.1.1
+// 4. Calculate the tip per person & display it
+// 5. Calculate bill per person + tips & display it 
+
+// Displaying
+//  Fields should output numbers trunked to 2 decimals.
+
+
+// ERROR MESSAGES:
+// PEOPLE CAN'T BE 0
+// PERCENTAGE CAN'T BE NEGATIVE
+// BILL CAN'T BE NEGATIVE
+// PEOPLE CAN'T BE NEGATIVE
